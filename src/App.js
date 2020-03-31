8f3817f85ddaddc8811c7d7dcbe639f46ef21b54
PineStone <mail@mikaahlsten.fi>
2020-03-31 22:16:48 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      		<header className="App-header">
+     	 	        <h1 style={{color: "red"}}>WELCOME!!!!!</h1>
+       			    <div>
+                 <p>
+                 Getting started with react
+        			   Join with Mika Ahlsten to this journey :)
+                </p>
+                </div>
+      		</header>
+     	</div>
+      
+
+  );
+}
+
+export default App;
